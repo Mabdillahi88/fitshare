a7c00edd2fd2f8ac8ac99d62fe60e512deea885e
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-08-13 20:21:36 +0000

    Updated App.js with FitShare welcome message

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to FitShare, Share Your Experiences and Wins!</h1>
+    </div>
+  );
+}
+
+export default App;
