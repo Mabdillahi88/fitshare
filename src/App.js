3a691820a2c56dde28de41de3c796a147ecef16f
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-08-14 19:48:15 +0000

    update to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to FitShare, Share Your Experiences and Wins!</h1>
+      <Button variant="primary">Click Me!</Button>
     </div>
   );
 }
