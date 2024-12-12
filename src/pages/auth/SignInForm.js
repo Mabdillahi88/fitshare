92d1716c14cb831c1c3d5d2eea0239c6c7826999
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-12-12 21:14:08 +0000

    login and signup pages ready

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,70 +1,72 @@
-import React from "react"; 
+import React from "react";
+
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+
 import { Link } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+function SignInForm() {
+//   Add your component logic here
 
-const SignInForm = () => {
   return (
     <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
+      <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>Sign in</h1>
-
-          {/* Sign-in form */}
+          <h1 className={styles.Header}>sign in</h1>
           <Form>
-            <Form.Group controlId="username">
-              <Form.Label className="d-none">Username</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Username"
-                name="username"
-                className={styles.Input}
-              />
-            </Form.Group>
+                <Form.Group controlId="username">
+                    <Form.Label className="d-none">Username</Form.Label>
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Username"
+                        name="username" 
+                        className={styles.Input}
+                    />
+                </Form.Group>
 
-            <Form.Group controlId="password">
-              <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                name="password"
-                className={styles.Input}
-              />
-            </Form.Group>
-
-            <Button
-              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-              type="submit"
-            >
-              Sign in
-            </Button>
-          </Form>
+                <Form.Group controlId="password">
+                    <Form.Label className="d-none">Password</Form.Label>
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Password" 
+                        name="password"
+                        className={styles.Input}
+                    />
+                </Form.Group>
+                <Button 
+                    className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+                    type="submit"
+                >
+                    Sign In
+                </Button>
+            </Form>
         </Container>
-
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
-            Don’t have an account? <span>Sign up</span>
+            Don't have an account? <span>Sign up now!</span>
           </Link>
         </Container>
       </Col>
-
       <Col
         md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
       >
         <Image
           className={`${appStyles.FillerImage}`}
-          src={
-            "https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"
-          }
+          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
         />
       </Col>
     </Row>
   );
-};
+}
 
 export default SignInForm;
