1b39d6f1df625979dfaf8e374940bcf9f15b422a
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2025-01-08 16:47:08 +0000

    Refactored PostsPage.js to refetch posts on user login/logout by adding useCurrentUser hook and updating useEffect dependencies.

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect, useState } from "react"; 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -17,11 +16,13 @@ import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
+import { useCurrentUser } from "../../contexts/CurrentUserContext"; // Import useCurrentUser
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
+  const currentUser = useCurrentUser(); // Use the hook
 
   const [query, setQuery] = useState("");
 
@@ -32,7 +33,7 @@ function PostsPage({ message, filter = "" }) {
         setPosts(data);
         setHasLoaded(true);
       } catch (err) {
-    //    console.log(err);
+        // console.log(err);
       }
     };
 
@@ -44,7 +45,7 @@ function PostsPage({ message, filter = "" }) {
     return () => {
       clearTimeout(timer);
     };
-  }, [filter, query, pathname]);
+  }, [filter, query, pathname, currentUser]); // Add currentUser here
 
   return (
     <Row className="h-100">
